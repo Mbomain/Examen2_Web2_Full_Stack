a951e8a8aa1f31061fbf43d7aa42a35ea8415f40
mbomain <ma.andriamparani@gmail.com>
2024-09-07 12:38:20 +0300

    Feat : add route for front

--- Backend/index.js
+++ Backend/index.js
@@ -6,15 +6,32 @@ import possessionRoutes from './Routes/possessionRoute.js';
 
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+  origin: 'https://front-g2tc.onrender.com', 
+  methods: 'GET,POST,PUT,DELETE',
+  allowedHeaders: 'Content-Type',
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send('Bienvenue sur l\'API');
-  }); 
+});
+
 app.use('/personnes', personneRoutes);
 app.use('/patrimoines', patrimoineRoutes);
 app.use('/possessions', possessionRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).json({ message: 'Route non trouvée' });
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: 'Erreur interne du serveur' });
+});
 
 const PORT = process.env.PORT || 5000;
 
